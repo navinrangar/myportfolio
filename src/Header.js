e24462c431233176e59c01b29453b0fa2f06e868
Navin Rangar <navinsinghrangar@gmail.com>
2022-04-12 19:53:32 +0530

    profile page  and header color

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ function Header(props){
     return( 
         <>
         
-        <div className="flex justify-between bg-yellow-800 pt-5 pb-2 pl-9 pr-10">
+        <div className="flex justify-between bg-red-800 pt-5 pb-2 pl-9 pr-10">
      <img className=" w-16 h-16 pb-4 " src="https://cool-klepon-aae5cf.netlify.app/assets/logo.edc03ba5.webp" />
             <div className="flex space-x-9 pt-2">
                 <NavLink className="text-xl"> Home </NavLink>
