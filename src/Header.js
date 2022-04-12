145ca95ffe8d17a24b6f75b140d48a8d3951b4de
Navin Rangar <navinsinghrangar@gmail.com>
2022-04-12 19:59:29 +0530

    changed home to front

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ function Header(props){
         <div className="flex justify-between bg-red-800 pt-5 pb-2 pl-9 pr-10">
      <img className=" w-16 h-16 pb-4 " src="https://cool-klepon-aae5cf.netlify.app/assets/logo.edc03ba5.webp" />
             <div className="flex space-x-9 pt-2">
-                <NavLink className="text-xl"> Home </NavLink>
+                <NavLink className="text-xl"> Front </NavLink>
                 <NavLink className="text-xl"> Projects </NavLink>
                 <NavLink className="text-xl"> Clients </NavLink>
                 <NavLink className="text-xl"> About </NavLink>
